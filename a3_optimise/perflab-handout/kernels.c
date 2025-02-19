5b01ba3e80dc40fc3223bd1d4427562ed63e7ba5
CORBIN ANTHONY BARNEY <corbbarney@gmail.com>
2025-02-19 11:41:12 -0700

    further development on motion algorithm

--- a3_optimise/perflab-handout/kernels.c
+++ a3_optimise/perflab-handout/kernels.c
@@ -49,141 +49,6 @@ void naive_complex(int dim, pixel *src, pixel *dest)
     }
 }
 
-
-// char precalculate_complex_descr[] = "precalculate_complex: Pre-calculate the values of the complex kernel";
-// void precalculate_complex(int dim, pixel *src, pixel *dest)
-// {
-//   int i, j;
-
-//   int srcI, destI;
-//   int gray;
-
-//   for(i = 0; i < dim; i++)
-//     for(j = 0; j < dim; j++)
-//     {
-//       srcI = RIDX(i, j, dim);
-//       destI = RIDX(dim - j - 1, dim - i - 1, dim);
-//       gray = (unsigned int)src[srcI].red;
-//       gray += (unsigned int)src[srcI].green;
-//       gray += (unsigned int)src[srcI].blue;
-//       gray /= 3;
-
-//       dest[destI].red = gray;
-//       dest[destI].green = gray;
-//       dest[destI].blue = gray;
-//     }
-// }
-
-// void tileAdjustable_complex(int dim, pixel *src, pixel *dest, int w)
-// {
-//   int i, j;
-//   int ii, jj;
-
-//   for(i = 0; i < dim; i += w)
-//     for(j = 0; j < dim; j += w)
-//     {
-//       for(ii = i; ii < i + w; ii++) {
-//         for(jj = j; jj < j + w; jj++) {
-
-//           dest[RIDX(dim - jj - 1, dim - ii - 1, dim)].red = ((int)src[RIDX(ii, jj, dim)].red +
-//               (int)src[RIDX(ii, jj, dim)].green +
-//               (int)src[RIDX(ii, jj, dim)].blue) / 3;
-
-//           dest[RIDX(dim - jj - 1, dim - ii - 1, dim)].green = ((int)src[RIDX(ii, jj, dim)].red +
-//               (int)src[RIDX(ii, jj, dim)].green +
-//               (int)src[RIDX(ii, jj, dim)].blue) / 3;
-
-//           dest[RIDX(dim - jj - 1, dim - ii - 1, dim)].blue = ((int)src[RIDX(ii, jj, dim)].red +
-//               (int)src[RIDX(ii, jj, dim)].green +
-//               (int)src[RIDX(ii, jj, dim)].blue) / 3;
-
-//         }
-//       }
-//     }
-// }
-
-// char tile32_complex_descr[] = "tile32_complex: tile the loop of the naive implementation with w=32";
-// void tile32_complex(int dim, pixel *src, pixel *dest)
-// {
-//   tileAdjustable_complex(dim, src, dest, 32);
-// }
-
-// char tile16_complex_descr[] = "tile16_complex: tile the loop of the naive implementation with w=16";
-// void tile16_complex(int dim, pixel *src, pixel *dest)
-// {
-//   tileAdjustable_complex(dim, src, dest, 16);
-// }
-
-// char tile08_complex_descr[] = "tile08_complex: tile the loop of the naive implementation with w=08";
-// void tile08_complex(int dim, pixel *src, pixel *dest)
-// {
-//   tileAdjustable_complex(dim, src, dest, 8);
-// }
-
-// char tile04_complex_descr[] = "tile04_complex: tile the loop of the naive implementation with w=04";
-// void tile04_complex(int dim, pixel *src, pixel *dest)
-// {
-//   tileAdjustable_complex(dim, src, dest, 4);
-// }
-
-// void comboAdjustable_complex(int dim, pixel *src, pixel *dest, int w)
-// {
-//   int i, j;
-//   int ii, jj;
-
-//   int srcI, destI;
-//   int gray;
-
-//   for(i = 0; i < dim; i += w) {
-//     for(j = 0; j < dim; j += w) {
-//       for(ii = i; ii < i + w; ii++) {
-//         for(jj = j; jj < j + w; jj++) {
-//           srcI = RIDX(ii, jj, dim);
-//           destI = RIDX(dim - jj - 1, dim - ii - 1, dim);
-//           gray = (unsigned int)src[srcI].red;
-//           gray += (unsigned int)src[srcI].green;
-//           gray += (unsigned int)src[srcI].blue;
-//           gray /= 3;
-
-//           dest[destI].red = gray;
-//           dest[destI].green = gray;
-//           dest[destI].blue = gray;
-//         }
-//       }
-//     }
-//   }
-// }
-
-// char combo32_complex_descr[] = "combo32_complex: Combine the precalculate and tile32 implementations";
-// void combo32_complex(int dim, pixel *src, pixel *dest)
-// {
-//   comboAdjustable_complex(dim, src, dest, 32);
-// }
-
-// char combo16_complex_descr[] = "combo16_complex: Combine the precalculate and tile16 implementations";
-// void combo16_complex(int dim, pixel *src, pixel *dest)
-// {
-//   comboAdjustable_complex(dim, src, dest, 16);
-// }
-
-// char combo8_complex_descr[] = "combo8_complex: Combine the precalculate and tile8 implementations";
-// void combo8_complex(int dim, pixel *src, pixel *dest)
-// {
-//   comboAdjustable_complex(dim, src, dest, 8);
-// }
-
-// char combo4_complex_descr[] = "combo4_complex: Combine the precalculate and tile4 implementations";
-// void combo4_complex(int dim, pixel *src, pixel *dest)
-// {
-//   comboAdjustable_complex(dim, src, dest, 4);
-// }
-
-// char combo2_complex_descr[] = "combo2_complex: Combine the precalculate and tile2 implementations";
-// void combo2_complex(int dim, pixel *src, pixel *dest)
-// {
-//   comboAdjustable_complex(dim, src, dest, 2);
-// }
-
 char unrolled_complex_dscr[] = "unrolled_complex: Combine a tile and unrolling algorithm to modify the image in patches that are 32row x 8col";
 void unrolled_complex(int dim, pixel *src, pixel *dest)
 {
@@ -338,7 +203,7 @@ static pixel weighted_combo(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_9square(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_9square(int dim, int i, int j, pixel *src) 
 {
   int i0, i1, i2;
   pixel current_pixel;
@@ -366,7 +231,7 @@ static pixel weighted_combo_9square(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_4square(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_4square(int dim, int i, int j, pixel *src) 
 {
   int i0, i1;
   pixel current_pixel;
@@ -390,7 +255,7 @@ static pixel weighted_combo_4square(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_6rectRight(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_6rectRight(int dim, int i, int j, pixel *src) 
 {
   int i0, i1, i2;
   pixel current_pixel;
@@ -418,7 +283,7 @@ static pixel weighted_combo_6rectRight(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_3rectRight(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_3rectRight(int dim, int i, int j, pixel *src) 
 {
   int i0, i1, i2;
   pixel current_pixel;
@@ -446,7 +311,7 @@ static pixel weighted_combo_3rectRight(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_2rectRight(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_2rectRight(int dim, int i, int j, pixel *src) 
 {
   int i0, i1;
   pixel current_pixel;
@@ -470,7 +335,7 @@ static pixel weighted_combo_2rectRight(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_6rectDown(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_6rectDown(int dim, int i, int j, pixel *src) 
 {
   int i0, i1;
   pixel current_pixel;
@@ -494,7 +359,7 @@ static pixel weighted_combo_6rectDown(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_3rectDown(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_3rectDown(int dim, int i, int j, pixel *src) 
 {
   int i0;
   pixel current_pixel;
@@ -514,7 +379,7 @@ static pixel weighted_combo_3rectDown(int dim, int i, int j, pixel *src)
   return current_pixel;
 }
 
-static pixel weighted_combo_2rectDown(int dim, int i, int j, pixel *src) 
+__attribute__((always_inline)) static pixel weighted_combo_2rectDown(int dim, int i, int j, pixel *src) 
 {
   int i0;
   pixel current_pixel;
@@ -606,22 +471,25 @@ void tile_motion(int dim, pixel *src, pixel *dst)
 {
   int i, j;
     
-  for (i = 0; i < dim-2; i++) {
-    for (j = 0; j < dim-2; j++) {
+  for (i = 0; i < dim-2; i += 2) {
+    for (j = 0; j < dim-2; j += 2) {
       dst[RIDX(i, j, dim)] = weighted_combo_9square(dim, i, j, src);
+      dst[RIDX(i, j+1, dim)] = weighted_combo_9square(dim, i, j+1, src);
+      dst[RIDX(i+1, j, dim)] = weighted_combo_9square(dim, i+1, j, src);
+      dst[RIDX(i+1, j+1, dim)] = weighted_combo_9square(dim, i+1, j+1, src);
     }
-  }
-
-  i = dim-2;
-  for (j = 0; j < dim-2; j++) {
-    dst[RIDX(i, j, dim)] = weighted_combo_6rectDown(dim, i, j, src);
-    dst[RIDX(i+1, j, dim)] = weighted_combo_3rectDown(dim, i+1, j, src);
-  }
-
-  j = dim-2;
-  for (i = 0; i < dim-2; i++) {
     dst[RIDX(i, j, dim)] = weighted_combo_6rectRight(dim, i, j, src);
     dst[RIDX(i, j+1, dim)] = weighted_combo_3rectRight(dim, i, j+1, src);
+    dst[RIDX(i+1, j, dim)] = weighted_combo_6rectRight(dim, i+1, j, src);
+    dst[RIDX(i+1, j+1, dim)] = weighted_combo_3rectRight(dim, i+1, j+1, src);
+  }
+  
+  i = dim-2;
+  for (j = 0; j < dim-2; j += 2) {
+    dst[RIDX(i, j, dim)] = weighted_combo_6rectDown(dim, i, j, src);
+    dst[RIDX(i+1, j, dim)] = weighted_combo_3rectDown(dim, i+1, j, src);
+    dst[RIDX(i, j+1, dim)] = weighted_combo_6rectDown(dim, i, j+1, src);
+    dst[RIDX(i+1, j+1, dim)] = weighted_combo_3rectDown(dim, i+1, j+1, src);
   }
 
   i = dim-2;
@@ -639,149 +507,6 @@ void tile_motion(int dim, pixel *src, pixel *dst)
   i = RIDX(dim-1, dim-1, dim);
   dst[i] = src[i];
 }
-
-// char inlineEdgeCase_motion_descr[] = "inlineEdgeCase_motion: Implementation that separates the edge cases out and inlines them.";
-// void inlineEdgeCase_motion(int dim, pixel *src, pixel *dst) 
-// {
-//   int i, j;
-//   int i0, i1, i2;
-//   int red, green, blue;
-//   int current;
-//
-//   for (i = 0; i < dim-2; i++) {
-//     for (j = 0; j < dim-2; j++) {
-//       current = RIDX(i, j, dim);
-//       i0 = RIDX(i, j, dim);
-//       i1 = RIDX(i+1, j, dim);
-//       i2 = RIDX(i+2, j, dim);
-//       red = (src[i0].red + src[i0+1].red + src[i0+2].red +
-//             src[i1].red + src[i1+1].red + src[i1+2].red +
-//             src[i2].red + src[i2+1].red + src[i2+2].red) / 9;
-//       green = (src[i0].green + src[i0+1].green + src[i0+2].green +
-//               src[i1].green + src[i1+1].green + src[i1+2].green +
-//               src[i2].green + src[i2+1].green + src[i2+2].green) / 9;
-//       blue = (src[i0].blue + src[i0+1].blue + src[i0+2].blue +
-//               src[i1].blue + src[i1+1].blue + src[i1+2].blue +
-//               src[i2].blue + src[i2+1].blue + src[i2+2].blue) / 9;
-//       dst[current].red = (unsigned short) red;
-//       dst[current].green = (unsigned short) green;
-//       dst[current].blue = (unsigned short) blue;
-//     }
-//   }
-//
-//   i = dim-2;
-//   for (j = 0; j < dim-2; j++) {
-//     current = RIDX(i, j, dim);
-//     i0 = RIDX(i, j, dim);
-//     i1 = RIDX(i+1, j, dim);
-//     red = (src[i0].red + src[i0+1].red + src[i0+2].red +
-//           src[i1].red + src[i1+1].red + src[i1+2].red) / 6;
-//     green = (src[i0].green + src[i0+1].green + src[i0+2].green +
-//             src[i1].green + src[i1+1].green + src[i1+2].green) / 6;
-//     blue = (src[i0].blue + src[i0+1].blue + src[i0+2].blue +
-//             src[i1].blue + src[i1+1].blue + src[i1+2].blue) / 6;
-//     dst[current].red = (unsigned short) red;
-//     dst[current].green = (unsigned short) green;
-//     dst[current].blue = (unsigned short) blue;
-//   }
-//
-//   i = dim-1;
-//   for (j = 0; j < dim-2; j++) {
-//     current = RIDX(i, j, dim);
-//     i0 = RIDX(i, j, dim);
-//     red = (src[i0].red + src[i0+1].red + src[i0+2].red) / 3;
-//     green = (src[i0].green + src[i0+1].green + src[i0+2].green) / 3;
-//     blue = (src[i0].blue + src[i0+1].blue + src[i0+2].blue) / 3;
-//     dst[current].red = (unsigned short) red;
-//     dst[current].green = (unsigned short) green;
-//     dst[current].blue = (unsigned short) blue;
-//   }
-//
-//   j = dim-2;
-//   for (i = 0; i < dim-2; i++) {
-//     current = RIDX(i, j, dim);
-//     i0 = RIDX(i, j, dim);
-//     i1 = RIDX(i+1, j, dim);
-//     i2 = RIDX(i+2, j, dim);
-//     red = (src[i0].red + src[i0+1].red +
-//           src[i1].red + src[i1+1].red +
-//           src[i2].red + src[i2+1].red) / 6;
-//     green = (src[i0].green + src[i0+1].green +
-//             src[i1].green + src[i1+1].green +
-//             src[i2].green + src[i2+1].green) / 6;
-//     blue = (src[i0].blue + src[i0+1].blue + 
-//             src[i1].blue + src[i1+1].blue +
-//             src[i2].blue + src[i2+1].blue) / 6;
-//     dst[current].red = (unsigned short) red;
-//     dst[current].green = (unsigned short) green;
-//     dst[current].blue = (unsigned short) blue;
-//   }
-//
-//   j = dim-1;
-//   for (i = 0; i < dim-2; i++) {
-//     current = RIDX(i, j, dim);
-//     i0 = RIDX(i, j, dim);
-//     i1 = RIDX(i+1, j, dim);
-//     i2 = RIDX(i+2, j, dim);
-//     red = (src[i0].red +
-//           src[i1].red +
-//           src[i2].red) / 3;
-//     green = (src[i0].green +
-//             src[i1].green +
-//             src[i2].green) / 3;
-//     blue = (src[i0].blue +
-//             src[i1].blue +
-//             src[i2].blue) / 3;
-//     dst[current].red = (unsigned short) red;
-//     dst[current].green = (unsigned short) green;
-//     dst[current].blue = (unsigned short) blue;
-//   }
-//
-//   i = dim-2;
-//   j = dim-2;
-//   current = RIDX(i, j, dim);
-//   i0 = RIDX(i, j, dim);
-//   i1 = RIDX(i+1, j, dim);
-//   red = (src[i0].red + src[i0+1].red +
-//         src[i1].red + src[i1+1].red) / 4;
-//   green = (src[i0].green + src[i0+1].green +
-//           src[i1].green + src[i1+1].green) / 4;
-//   blue = (src[i0].blue + src[i0+1].blue +
-//           src[i1].blue + src[i1+1].blue) / 4;
-//   dst[current].red = (unsigned short) red;
-//   dst[current].green = (unsigned short) green;
-//   dst[current].blue = (unsigned short) blue;
-//
-//   i = dim-1;
-//   j = dim-2;
-//   current = RIDX(i, j, dim);
-//   i0 = RIDX(i, j, dim);
-//   red = (src[i0].red + src[i0+1].red) / 2;
-//   green = (src[i0].green + src[i0+1].green) / 2;
-//   blue = (src[i0].blue + src[i0+1].blue) / 2;
-//   dst[current].red = (unsigned short) red;
-//   dst[current].green = (unsigned short) green;
-//   dst[current].blue = (unsigned short) blue;
-//
-//   i = dim-2;
-//   j = dim-1;
-//   current = RIDX(i, j, dim);
-//   i0 = RIDX(i, j, dim);
-//   i1 = RIDX(i+1, j, dim);
-//   red = (src[i0].red +
-//          src[i1].red) / 2;
-//   green = (src[i0].green +
-//            src[i1].green) / 2;
-//   blue = (src[i0].blue +
-//           src[i1].blue) / 2;
-//   dst[current].red = (unsigned short) red;
-//   dst[current].green = (unsigned short) green;
-//   dst[current].blue = (unsigned short) blue;
-//
-//   i = RIDX(dim-1, dim-1, dim);
-//   dst[i] = src[i];
-// }
-
 
 /*
  * motion - Your current working version of motion. 
@@ -804,7 +529,6 @@ void motion(int dim, pixel *src, pixel *dst)
 void register_motion_functions() {
   add_motion_function(&motion, motion_descr);
   add_motion_function(&edgeCase_motion, edgeCase_motion_descr);
-  // add_motion_function(&inlineEdgeCase_motion, inlineEdgeCase_motion_descr);
   add_motion_function(&tile_motion, tile_motion_descr);
   add_motion_function(&naive_motion, naive_motion_descr);
 }
